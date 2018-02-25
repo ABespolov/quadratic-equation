12a9a3907854607dd58ff377851ef1b9c924cfa4
ABespolov <abespolov@gmail.com>
2018-02-25 22:32:07 +0300

    feature: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+    equation = equation.split('').filter(item => /\S/.test(item));
+    equation = equation.join('');
+
+    function solve(a, b, c) {
+
+        a = parseInt(a);
+        b = parseInt(b);
+        c = parseInt(c);
+
+        var result = (-1 * b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+        var result2 = (-1 * b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
+        return [Math.round(result), Math.round(result2)].sort((a, b) => a - b);
+    }
+
+    var match = equation.match(/[-\+]\d+|(^\d+)/g);
+
+    match = match.filter(item => item);
+    return solve(...match);
+
 }
